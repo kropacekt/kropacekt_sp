868292219d87ea9aca2f539b2635f325e8d6cf9f
Tomáš Kropáček <kropacekt@email.cz>
2017-05-23 21:51:28 +0200

    tabledit-osetreni-ceny

--- migrace.php
+++ migrace.php
@@ -6,5 +6,11 @@
  * Time: 17:10
  */
 
+require_once 'inc/connection.inc.php';
+
 $sql[] = "ALTER TABLE zbozi ADD smazano SET('0', '1') AFTER cena";
 $sql[] = "UPDATE zbozi SET smazano = '0'";
+
+foreach($sql as $prikaz) {
+    mysqli_query($link, $prikaz);
+}
--- tpl/js/filtrace.ajax.js
+++ tpl/js/filtrace.ajax.js
@@ -6,6 +6,7 @@ $(function () {
     //http://stackoverflow.com/questions/5004233/jquery-ajax-post-example-with-php
 
     $("#filtrace").submit(function(event) {
+
         event.preventDefault(); //zablokuje poslani formu
         var serializedData = $(this).serialize();
         //console.log(serializedData);
@@ -21,13 +22,21 @@ $(function () {
 
             complete: function() {
                 $('.modal').hide();
-                tableEdit();
+                tableEdit(); //fce ze souboru edit.tableEdit.js
+
+                //defaultne se pres tabledit nastavuje vsem inputum type="text", timto kodem osetruji, aby cena byla zadana jako cislo
+                $('.tabledit-input').each(function() {
+                    if($(this).attr('name') === "cena") {
+                         $(this).attr('type', 'number');
+                    }
+                });
             },
 
             success: function(data) {
                 $("#vysledekFiltrace > tbody").html("");
 
                 $.each(data, function(index, vyrobek) {
+                    //bylo by lepsi toto resit v backendu, browser jinak pri velkem poctu zaznamu zamrzne
                     $('#vysledekFiltrace tbody').append("<tr><td>" + vyrobek.id + "</td><td>" + vyrobek.nazev + "</td><td>" + vyrobek.jednotky + "</td><td>" + vyrobek.cena + "</td></tr>");
                 });
 
